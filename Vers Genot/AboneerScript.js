c7d689d20334c9b5e51be6425dfa63061d458c1d
Mechak01 <152030992+Mechak01@users.noreply.github.com>
2023-11-25 20:39:40 +0100

    Add files via upload

---
+++ Vers Genot/AboneerScript.js
@@ -0,0 +1,37 @@
+const name = document.querySelector(".FirstPage")
+
+function functionToExecute1()
+{
+    window.open("SubscriptionsOptions.html")
+}
+
+function functionToExecute2()
+{
+    window.open("AboneerProcedure2.html")
+    window.close();
+   
+}
+
+function functionToExecute4()
+{
+    window.open("AboneerProcedure.html")
+    window.close();
+}
+
+function functionToExecute5()
+{
+    window.open("AboneerProcedure.html")
+    window.close();
+}
+
+
+function functionToExecute6()
+{
+    window.open("AboneerProcedure.html")
+    window.close();
+}
+
+
+
+
+
